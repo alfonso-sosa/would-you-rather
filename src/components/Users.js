c14cae7a66b8848b19cfa063855b5530d568a1a9
Alfonso Sosa <alfonso.sosa@gmail.com>
2018-06-20 22:49:59 -0700

    User list

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,16 +1,41 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, { Component, Fragment } from 'react'
+import { Grid, Row, Col, Panel } from 'react-bootstrap'
+import { connect } from 'react-redux'
 
 
 class Users extends Component {
   render() {
+    const { users } = this.props;
+    console.log(users);
     return (
-      <div>Users</div>
+      <Fragment>
+        <br />
+        <Grid>
+          {
+            Object.keys(users).map((key) => {
+              const user = users[key];
+              return (
+                <Row>
+                  <Col xs={12} md={8}>
+                    <Panel>
+                      <Panel.Heading>
+                        <Panel.Title componentClass="h3"></Panel.Title>
+                      </Panel.Heading>
+                      <Panel.Body>{user.name}</Panel.Body>
+                    </Panel>
+                  </Col>
+                </Row>
+              )
+            })
+          }
+
+        </Grid>
+      </Fragment>
     )
   }
 }
 
-function mapStateToProps({users}) {
+function mapStateToProps({ users }) {
   return {
     users
   }
